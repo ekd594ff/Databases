c455b90a698d16e6b07e3c4bd7cf1f5cd7190a2d
OOO <94286943+ekd594ff@users.noreply.github.com>
2024-08-02 15:44:28 +0900

    Update OrdersMaxEmployee.sql

--- sql/OrdersMaxEmployee.sql
+++ sql/OrdersMaxEmployee.sql
@@ -1,10 +1,10 @@
 SELECT LastName, FirstName, Total
 FROM (SELECT e.LastName, e.FirstName, count(o.OrderID) AS Total
-FROM Orders o, Employees e 
-WHERE o.EmployeeID=e.EmployeeID 
+FROM Orders o INNER JOIN Employees e 
+ON o.EmployeeID=e.EmployeeID 
 GROUP BY e.LastName, e.FirstName)
 WHERE Total >= All (
 SELECT count(o.OrderID) AS Total
-FROM Orders o, Employees e 
-WHERE o.EmployeeID=e.EmployeeID 
+FROM Orders o INNER JOIN Employees e 
+ON o.EmployeeID=e.EmployeeID 
 GROUP BY e.LastName, e.FirstName);
